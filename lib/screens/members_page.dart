de6610dff78c71f6df88fec5c9fdbdaa9636c4e3
Hishamk99 <hishamkamel370@gmail.com>
2023-09-29 15:09:36 +0200

    create members page ui for one item

--- lib/screens/members_page.dart
+++ lib/screens/members_page.dart
@@ -7,7 +7,54 @@ class MembersPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        
+        appBar: AppBar(
+          backgroundColor: const Color(0xff46322b),
+          title: Text('Members'),
+        ),
+        body: Container(
+          height: 100,
+          color: const Color(0xffef9235),
+          child: Row(
+            children: [
+              Container(
+                  color: Color(0xfffff6dc),
+                  child: Image.asset('assets/images/numbers/number_one.png')),
+              Padding(
+                padding: const EdgeInsets.only(left: 16),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Ichi',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 18,
+                      ),
+                    ),
+                    Text(
+                      'One',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 18,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Spacer(
+                flex: 1,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(right: 16),
+                child: Icon(
+                  Icons.play_arrow,
+                  color: Colors.white,
+                  size: 32,
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
